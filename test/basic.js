1512af695f2449fe483ff654a2ae539c1d054ba4
Feross Aboukhadijeh <feross@feross.org>
2014-04-09 23:52:59 -0700

    fix typed array check

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var convertToBuffer = require('../')
 
 test('convert to buffer from uint8array', function (t) {
-  if (typeof Uint8Array === 'function') {
+  if (typeof Uint8Array !== 'undefined') {
     var arr = new Uint8Array([1, 2, 3])
     arr = convertToBuffer(arr)
 
@@ -16,7 +16,7 @@ test('convert to buffer from uint8array', function (t) {
 })
 
 test('convert to buffer from another array type (uint32array)', function (t) {
-  if (typeof Uint32Array === 'function') {
+  if (typeof Uint32Array !== 'undefined') {
     var arr = new Uint32Array([1, 2, 3])
     arr = convertToBuffer(arr)
 
