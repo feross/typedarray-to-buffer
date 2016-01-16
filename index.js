5d7ece3148dca1eb5a4644c750f44773c5d2d06b
Jan Schär <jscissr@gmail.com>
2016-01-16 21:14:01 +0100

    Passing an ArrayBuffer to new Buffer() is efficient
    Now this module is independent from any `buffer` internals.

--- index.js
+++ index.js
@@ -6,28 +6,25 @@
  *
  * `npm install typedarray-to-buffer`
  */
- /* eslint-disable no-proto */
 
 var isTypedArray = require('is-typedarray').strict
 
 module.exports = function (arr) {
   // If `Buffer` is the browser `buffer` module, and the browser supports typed arrays,
   // then avoid a copy. Otherwise, create a `Buffer` with a copy.
-  var constructor = Buffer.TYPED_ARRAY_SUPPORT
-    ? function (arr) {
-      arr.__proto__ = Buffer.prototype
-      return arr
+  if (isTypedArray(arr)) {
+    if (!Buffer.TYPED_ARRAY_SUPPORT) {
+      // This is not faster than new Buffer(arr), but it has different semantics,
+      // because it uses 4 bytes instead of one per element for Uint32Array.
+      return new Buffer(new Uint8Array(arr.buffer, arr.byteOffset, arr.byteLength))
     }
-    : function (arr) { return new Buffer(arr) }
-
-  if (arr instanceof Uint8Array) {
-    return constructor(arr)
-  } else if (arr instanceof ArrayBuffer) {
-    return constructor(new Uint8Array(arr))
-  } else if (isTypedArray(arr)) {
     // Use the typed array's underlying ArrayBuffer to back new Buffer. This respects
     // the "view" on the ArrayBuffer, i.e. byteOffset and byteLength. No copy.
-    return constructor(new Uint8Array(arr.buffer, arr.byteOffset, arr.byteLength))
+    var buffer = new Buffer(arr.buffer)
+    if (arr.byteLength !== buffer.length) {
+      buffer = buffer.slice(arr.byteOffset, arr.byteOffset + arr.byteLength)
+    }
+    return buffer
   } else {
     // Unsupported type, just pass it through to the `Buffer` constructor.
     return new Buffer(arr)
